44a3d707ce1c43ba0c4f6fdaca7c8701065929f5
Edward Han <edwrdhn@gmail.com>
2024-09-01 15:06:44 -0400

    removing extra files, protected route, access to prototype page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PrototypePage from './pages/prototype';
-import ProtectedRoute from './pages/protectedRoute'
 import LandingPageComponent from './pages/landingPage';
 import Confirmation from './pages/confirmationPage';
 import './App.css';
