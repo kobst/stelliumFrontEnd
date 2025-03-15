5e3003774f47a2932577df7dd813bc7440140878
Edward Han <edwrdhn@gmail.com>
2025-03-15 19:20:48 -0400

    password protect

--- src/App.js
+++ src/App.js
@@ -7,21 +7,24 @@ import Confirmation from './pages/confirmationPage';
 import UserDashboard from './pages/userDashboard';
 import SynastryPage from './pages/synastryPage';
 import CompositeDashboard from './pages/compositeDashboard';
+import PasswordProtection from './pages/passwordProtection';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPageComponent />} />
-          <Route path="/admin" element={<LandingPageAdmin />} />
-          <Route path="/confirmation" element={<Confirmation />} />
-          <Route path="/prototype" element={<PrototypePage />} />
-          <Route path="/userDashboard" element={<UserDashboard />} />
+        <PasswordProtection>
+          <Routes>
+            <Route path="/" element={<LandingPageComponent />} />
+            <Route path="/admin" element={<LandingPageAdmin />} />
+            <Route path="/confirmation" element={<Confirmation />} />
+            <Route path="/prototype" element={<PrototypePage />} />
+            <Route path="/userDashboard" element={<UserDashboard />} />
           <Route path="/synastry" element={<SynastryPage />} />
-          <Route path="/compositeDashboard" element={<CompositeDashboard />} />
-        </Routes>
+            <Route path="/compositeDashboard" element={<CompositeDashboard />} />
+          </Routes>
+        </PasswordProtection>
       </div>
     </Router>
   );
