d55e6052c7c56ad75476bd0aee0b820a3e335ce4
Edward Han <edwrdhn@gmail.com>
2025-03-15 19:20:48 -0400

    password protect

--- src/App.js
+++ src/App.js
@@ -8,26 +8,30 @@ import UserDashboard from './pages/userDashboard';
 import SynastryPage_v2 from './pages/synastryPage_v2';
 import CompositeDashboard_v4 from './pages/compositeDashboard_v4';
 import ConfirmationV2 from './pages/confirmationPage_v2';
+import PasswordProtection from './pages/passwordProtection';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
-          {/* <Route path="/" element={<LandingPageComponent />} /> */}
-          {/* <Route path="/admin" element={<LandingPageAdmin />} /> */}
-          {/* <Route path="/confirmation" element={<Confirmation />} /> */}
-          <Route path="/prototype" element={<PrototypePage />} />
-          <Route path="/userDashboard" element={<UserDashboard />} />
-          <Route path="/synastry" element={<SynastryPage_v2 />} />
-          <Route path="/compositeDashboard" element={<CompositeDashboard_v4 />} />
+        <PasswordProtection>
+          <Routes>
+            {/* <Route path="/" element={<LandingPageComponent />} /> */}
+            {/* <Route path="/admin" element={<LandingPageAdmin />} /> */}
+            {/* <Route path="/confirmation" element={<Confirmation />} /> */}
+            <Route path="/prototype" element={<PrototypePage />} />
+            <Route path="/userDashboard" element={<UserDashboard />} />
+            <Route path="/synastry" element={<SynastryPage_v2 />} />
+            <Route path="/compositeDashboard" element={<CompositeDashboard_v4 />} />
 
-          <Route path="/signUp" element={<SignUpPage />} />
-          <Route path="/signUpConfirmation" element={<ConfirmationV2 />} />
+            <Route path="/signUp" element={<SignUpPage />} />
+            <Route path="/signUpConfirmation" element={<ConfirmationV2 />} />
 
 
-        </Routes>
+          </Routes>
+        </PasswordProtection>
+
       </div>
     </Router>
   );
