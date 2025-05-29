32fe8205c8e0605f8a54cd45688d908f43a40a71
Edward Han <edwrdhn@gmail.com>
2025-05-28 20:08:01 -0400

    ephemeris change to separate out obstructed planets

---
+++ src/UI/shared/EphemerisConstants.tsx
@@ -0,0 +1,51 @@
+  
+export const zodiacIcons = [
+    '/assets/signs/aries.svg',
+    '/assets/signs/taurus.svg',
+    '/assets/signs/gemini.svg',
+    '/assets/signs/cancer.svg',
+    '/assets/signs/leo.svg',
+    '/assets/signs/virgo.svg',
+    '/assets/signs/libra.svg',
+    '/assets/signs/scorpio.svg',
+    '/assets/signs/sagittarius.svg',
+    '/assets/signs/capricorn.svg',
+    '/assets/signs/aquarius.svg',
+    '/assets/signs/pisces.svg'
+];
+
+
+export const planetIcons = [
+    '/assets/planets/Sun.svg',
+    '/assets/planets/Moon.svg',
+    '/assets/planets/Mercury.svg',
+    '/assets/planets/Venus.svg',
+    '/assets/planets/Mars.svg',
+    '/assets/planets/Jupiter.svg',
+    '/assets/planets/Saturn.svg',
+    '/assets/planets/Uranus.svg',
+    '/assets/planets/Neptune.svg',
+    '/assets/planets/Pluto.svg'
+]
+
+
+export const planetNameToIndex = {
+    "Sun": 0,
+    "Moon": 1,
+    "Mercury": 2,
+    "Venus": 3,
+    "Mars": 4,
+    "Jupiter": 5,
+    "Saturn": 6,
+    "Uranus": 7,
+    "Neptune": 8,
+    "Pluto": 9,
+};
+
+export const CANVAS_DIMENSIONS = {
+    centerX: 300,
+    centerY: 300,
+    outerRadius: 170,
+    innerRadius: 105,
+    houseCircleRadius: 180
+};
